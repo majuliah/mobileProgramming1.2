b78896765455aeb6b7fcc1348143bad4c5d42320
Majulia <majulialcarvalho@gmail.com>
2021-10-11 20:36:01 -0300

    aula do dia 11

--- App.tsx
+++ App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useFonts } from 'expo-font';
+import { Rajdhani_300Light, Rajdhani_400Regular, Rajdhani_500Medium, Rajdhani_600SemiBold} from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components/native';
 import { Signin } from './src/pages/Signin';
 import { theme } from './src/global/styles/theme';
 
+
+
 export default function App() {
+  const [fontsLoaded] =  useFonts({Rajdhani_600SemiBold, Rajdhani_500Medium, Rajdhani_400Regular, Rajdhani_300Light})
+  if(!fontsLoaded){
+    return<AppLoading/>
+  }
+  
   return (
     <>
     <ThemeProvider theme={theme}>
--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -4,5 +4,9 @@ export const theme = {
         heading: '#DDE3F0',
         primary: '#E51C44',
         line: '#991F36'
-  }
+  },
+    fonts: {
+      title700: 'Rajdhani_700Bold',
+      title500: 'Rajdhani_500Medium',
+    }
 }
